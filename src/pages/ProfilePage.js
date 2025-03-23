fe66cad930d7348342002dc0eced39c8a584fee0
kutta97 <yhjin04170@gmail.com>
2025-03-23 22:33:43 +0900

    feat: 프로필 정보 입력 폼에 id 추가

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -11,7 +11,7 @@ const ProfilePage = ({ user }) => `
             <h2 class="text-2xl font-bold text-center text-blue-600 mb-8">
               내 프로필
             </h2>
-            <form>
+            <form id="profile-form">
               <div class="mb-4">
                 <label
                   for="username"
