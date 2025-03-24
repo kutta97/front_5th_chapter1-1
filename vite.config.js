0b6479d54e5e5c8a3cb9268441da3ef799236ec1
kutta97 <yhjin04170@gmail.com>
2025-03-25 00:40:52 +0900

    fix: vite base 설정 위치 잘못된 것 수정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/front_5th_chapter1-1/",
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    base: "/front_5th_chapter1-1/",
   },
 });
